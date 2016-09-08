c9825ece37e27af4146193fec4d391ee0c97fc30
tanshaohui <tanshaohui@baidu.com>
2016-09-08 11:16:22 +0800

    flv parse AudioSpecificConfig

--- src/demux/flvdemuxer.js
+++ src/demux/flvdemuxer.js
@@ -4,13 +4,13 @@
  * @email:  tanshaohui@baidu.com
  * @date:   2016-09-07 10:23:57
  * @last modified by:   tanshaohui
- * @last modified time: 2016-09-07 22:15:27
+ * @last modified time: 2016-09-08 11:14:56
  */
 
-import ADTS from './adts';
 import FLVParser from './flv/flv-parser';
 import FLVTag from './flv/flv-tag';
 import {logger} from '../utils/logger';
+import {ErrorTypes, ErrorDetails} from '../errors';
 
 class FLVDemuxer {
 
@@ -97,15 +97,15 @@ class FLVDemuxer {
                     let tag = cTag.readData(data.slice(start, start + cTag.tagDataSize));
                     if (tag && tag.codec) {
                         if (tag.codec === 'aac') {
-                            if (tag.pkt_type === 1) {
+                            let track = this._aacTrack;
+                            if (tag.pkt_type === 1 && track.audiosamplerate) {
                                 this._parseAACTag(tag);
-                            } else if (tag.pkt_type === 0 && !this._aacTrack.audiosamplerate) {
-                                let track = this._aacTrack;
-                                let config = ADTS.getAudioConfig(this.observer, tag.data, -2, audioCodec);
+                            } else if (tag.pkt_type === 0 && !track.audiosamplerate) {
+                                let config = this.getAudioConfig(this.observer, tag.data, 0, audioCodec);
+                                track.config = config.config;
                                 track.audiosamplerate = config.samplerate;
                                 track.channelCount = config.channelCount;
                                 track.codec = config.codec;
-                                track.config = [41, 145, 136, 0];
                                 track.duration = this._duration; 
                             }
                         } else if (tag.codec === 'avc') {
@@ -252,6 +252,127 @@ class FLVDemuxer {
             this.avcNaluState = state;
         }
         return units;
+    }
+
+    getAudioConfig (observer, data, offset, audioCodec) {
+        var adtsObjectType, // :int
+            adtsSampleingIndex, // :int
+            adtsExtensionSampleingIndex, // :int
+            adtsChanelConfig, // :int
+            config,
+            userAgent = navigator.userAgent.toLowerCase(),
+            adtsSampleingRates = [
+                96000, 88200,
+                64000, 48000,
+                44100, 32000,
+                24000, 22050,
+                16000, 12000,
+                11025, 8000,
+                7350
+            ];
+
+        adtsObjectType = (data[0] & 0xF8) >> 3;
+        adtsSampleingIndex = ((data[0] & 0x7) << 1) | (data[1] >> 7);
+        if (adtsSampleingIndex > adtsSampleingRates.length - 1) {
+            observer.trigger(Event.ERROR, { type: ErrorTypes.MEDIA_ERROR, details: ErrorDetails.FRAG_PARSING_ERROR, fatal: true, reason: `invalid ADTS sampling index:${adtsSampleingIndex}` });
+            return;
+        }
+        adtsChanelConfig = (data[1] >> 3) & 0x0F;
+        logger.log(`manifest codec:${audioCodec},ADTS data:type:${adtsObjectType},sampleingIndex:${adtsSampleingIndex}[${adtsSampleingRates[adtsSampleingIndex]}Hz],channelConfig:${adtsChanelConfig}`);
+        // firefox: freq less than 24kHz = AAC SBR (HE-AAC)
+        if (userAgent.indexOf('firefox') !== -1) {
+            if (adtsSampleingIndex >= 6) {
+                adtsObjectType = 5;
+                config = new Array(4);
+                // HE-AAC uses SBR (Spectral Band Replication) , high frequencies are constructed from low frequencies
+                // there is a factor 2 between frame sample rate and output sample rate
+                // multiply frequency by 2 (see table below, equivalent to substract 3)
+                adtsExtensionSampleingIndex = adtsSampleingIndex - 3;
+            } else {
+                adtsObjectType = 2;
+                config = new Array(2);
+                adtsExtensionSampleingIndex = adtsSampleingIndex;
+            }
+            // Android : always use AAC
+        } else if (userAgent.indexOf('android') !== -1) {
+            adtsObjectType = 2;
+            config = new Array(2);
+            adtsExtensionSampleingIndex = adtsSampleingIndex;
+        } else {
+            /*  for other browsers (chrome ...)
+                always force audio type to be HE-AAC SBR, as some browsers do not support audio codec switch properly (like Chrome ...)
+            */
+            adtsObjectType = 5;
+            config = new Array(4);
+            // if (manifest codec is HE-AAC or HE-AACv2) OR (manifest codec not specified AND frequency less than 24kHz)
+            if ((audioCodec && ((audioCodec.indexOf('mp4a.40.29') !== -1) ||
+                    (audioCodec.indexOf('mp4a.40.5') !== -1))) ||
+                (!audioCodec && adtsSampleingIndex >= 6)) {
+                // HE-AAC uses SBR (Spectral Band Replication) , high frequencies are constructed from low frequencies
+                // there is a factor 2 between frame sample rate and output sample rate
+                // multiply frequency by 2 (see table below, equivalent to substract 3)
+                adtsExtensionSampleingIndex = adtsSampleingIndex - 3;
+            } else {
+                // if (manifest codec is AAC) AND (frequency less than 24kHz AND nb channel is 1) OR (manifest codec not specified and mono audio)
+                // Chrome fails to play back with low frequency AAC LC mono when initialized with HE-AAC.  This is not a problem with stereo.
+                if (audioCodec && audioCodec.indexOf('mp4a.40.2') !== -1 && (adtsSampleingIndex >= 6 && adtsChanelConfig === 1) ||
+                    (!audioCodec && adtsChanelConfig === 1)) {
+                    adtsObjectType = 2;
+                    config = new Array(2);
+                }
+                adtsExtensionSampleingIndex = adtsSampleingIndex;
+            }
+        }
+        /* refer to http://wiki.multimedia.cx/index.php?title=MPEG-4_Audio#Audio_Specific_Config
+            ISO 14496-3 (AAC).pdf - Table 1.13 — Syntax of AudioSpecificConfig()
+          Audio Profile / Audio Object Type
+          0: Null
+          1: AAC Main
+          2: AAC LC (Low Complexity)
+          3: AAC SSR (Scalable Sample Rate)
+          4: AAC LTP (Long Term Prediction)
+          5: SBR (Spectral Band Replication)
+          6: AAC Scalable
+         sampling freq
+          0: 96000 Hz
+          1: 88200 Hz
+          2: 64000 Hz
+          3: 48000 Hz
+          4: 44100 Hz
+          5: 32000 Hz
+          6: 24000 Hz
+          7: 22050 Hz
+          8: 16000 Hz
+          9: 12000 Hz
+          10: 11025 Hz
+          11: 8000 Hz
+          12: 7350 Hz
+          13: Reserved
+          14: Reserved
+          15: frequency is written explictly
+          Channel Configurations
+          These are the channel configurations:
+          0: Defined in AOT Specifc Config
+          1: 1 channel: front-center
+          2: 2 channels: front-left, front-right
+        */
+        // audioObjectType = profile => profile, the MPEG-4 Audio Object Type minus 1
+        config[0] = adtsObjectType << 3;
+        // samplingFrequencyIndex
+        config[0] |= (adtsSampleingIndex & 0x0E) >> 1;
+        config[1] |= (adtsSampleingIndex & 0x01) << 7;
+        // channelConfiguration
+        config[1] |= adtsChanelConfig << 3;
+        if (adtsObjectType === 5) {
+            // adtsExtensionSampleingIndex
+            config[1] |= (adtsExtensionSampleingIndex & 0x0E) >> 1;
+            config[2] = (adtsExtensionSampleingIndex & 0x01) << 7;
+            // adtsObjectType (force to 2, chrome is checking that object type is less than 5 ???
+            //    https://chromium.googlesource.com/chromium/src.git/+/master/media/formats/mp4/aac.cc
+            config[2] |= 2 << 2;
+            config[3] = 0;
+        }
+        return { config: config, samplerate: adtsSampleingRates[adtsSampleingIndex], channelCount: adtsChanelConfig, codec: ('mp4a.40.' + adtsObjectType) };
     }
 
 }
